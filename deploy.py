4afe1696c3480274a408194a1c17c4e0db274ce8
Sithvothy KIV <34917417+sithvothykiv@users.noreply.github.com>
2023-03-29 15:49:14 +0700

    deploy: app project and docker-compose

--- deploy.py
+++ deploy.py
@@ -1,22 +1,39 @@
 import sys
+import os
 
 
-def switch_app_deployment(deployment_arg):
+def build_deployment(deployment_arg):
     match deployment_arg:
         case "app1":
-            print("Gonna deploy app1")
+            print("Building " + deployment_arg + ".....")
+            os.system('cd ProjectsApp && cd financial-service-web && docker build -t financial-service-web .')
+            os.system('docker stack deploy node --compose-file docker-compose.yml --with-registry-auth')
+            # os.system('docker stack rm node')
         case "app2":
-            print("Gonna deploy app2")
+            print("Building " + deployment_arg + ".....")
+            os.system('cd ProjectsApp && cd pet-shop-web && docker build -t pet-shop-web:v1.0.6 .')
         case "app3":
-            print("Gonna deploy app3")
-        case _:
-            print("")
+            print("Building " + deployment_arg + ".....")
+            os.system('cd ProjectsApp && cd selo-web && docker build -t selo-web .')
+        case "all":
+            print("Building " + deployment_arg + ".....")
+            # build_all_deployment()
+            os.system('cd ProjectsApp && cd financial-service-web && docker build -t financial-service-web .')
+            os.system('cd ProjectsApp && cd pet-shop-web && docker build -t pet-shop-web .')
+            os.system('cd ProjectsApp && cd selo-web && docker build -t selo-web .')
 
 
 def deploy():
     for app in sys.argv[1:]:
-        switch_app_deployment(app)
-    print('Argument List: ', str(sys.argv))
+        build_deployment(app)
+
+    # start swarm services
+    # docker stack deploy node --compose-file docker-compose.yml --with-registry-auth
+
+    # stop swarm services
+    # docker stack rm node
+
+    # print('Argument List: ', str(sys.argv))
 
 
 if __name__ == '__main__':
