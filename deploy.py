03bb42823cb09e19633a55c9efda65eb7d88881f
Sithvothy KIV <34917417+sithvothykiv@users.noreply.github.com>
2023-03-31 09:55:11 +0700

    deploy: deployment cmd

--- deploy.py
+++ deploy.py
@@ -7,19 +7,22 @@ def build_deployment(deployment_arg):
         case "app1":
             print("Building " + deployment_arg + ".....")
             os.system('cd ProjectsApp && cd financial-service-web && docker build -t financial-service-web .')
-            os.system('docker stack deploy node --compose-file docker-compose.yml --with-registry-auth')
-            # os.system('docker stack rm node')
+            os.system('docker stack deploy node --compose-file docker-compose-microservices.yml --with-registry-auth')
         case "app2":
             print("Building " + deployment_arg + ".....")
-            os.system('cd ProjectsApp && cd pet-shop-web && docker build -t pet-shop-web:v1.0.6 .')
+            os.system('cd ProjectsApp && cd pet-shop-web && docker build -t pet-shop-web .')
+            os.system('docker stack deploy node --compose-file docker-compose-microservices.yml --with-registry-auth')
+
         case "app3":
             print("Building " + deployment_arg + ".....")
             os.system('cd ProjectsApp && cd selo-web && docker build -t selo-web .')
+            os.system('docker stack deploy node --compose-file docker-compose-microservices.yml --with-registry-auth')
         case "all":
             print("Building " + deployment_arg + ".....")
             os.system('cd ProjectsApp && cd financial-service-web && docker build -t financial-service-web .')
             os.system('cd ProjectsApp && cd pet-shop-web && docker build -t pet-shop-web .')
             os.system('cd ProjectsApp && cd selo-web && docker build -t selo-web .')
+            os.system('docker stack deploy node --compose-file docker-compose-microservices.yml --with-registry-auth')
 
 
 def deploy():
