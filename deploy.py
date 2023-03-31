bdf5855b92e03b28d6936d687e393cacd8a61c6f
Sithvothy KIV <34917417+sithvothykiv@users.noreply.github.com>
2023-03-31 11:50:23 +0700

    deploy: replicas

--- deploy.py
+++ deploy.py
@@ -17,10 +17,16 @@ def build_deployment(deployment_arg):
             print("Building " + deployment_arg + ".....")
             os.system('cd ProjectsApp && cd selo-web && docker build -t selo-web .')
             os.system('docker stack deploy node --compose-file docker-compose-microservices.yml --with-registry-auth')
+        case "app4":
+            print("Building " + deployment_arg + ".....")
+            os.system('cd ProjectsApp && cd php-sample-demo && docker build -t php-sample-demo .')
+            os.system('docker stack deploy node --compose-file docker-compose-microservices.yml --with-registry-auth')
+
         case "all":
             print("Building " + deployment_arg + ".....")
             os.system('cd ProjectsApp && cd financial-service-web && docker build -t financial-service-web .')
             os.system('cd ProjectsApp && cd pet-shop-web && docker build -t pet-shop-web .')
+            os.system('cd ProjectsApp && cd php-sample-demo && docker build -t php-sample-demo .')
             os.system('cd ProjectsApp && cd selo-web && docker build -t selo-web .')
             os.system('docker stack deploy node --compose-file docker-compose-microservices.yml --with-registry-auth')
 
