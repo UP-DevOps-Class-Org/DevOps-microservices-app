fd34a940e5074ae2772e9d17f5a57372b2563cea
Sithvothy KIV <34917417+sithvothykiv@users.noreply.github.com>
2023-04-06 09:26:45 +0700

    delete uesless file

--- deploy.py
+++ deploy.py
@@ -6,29 +6,29 @@ def build_deployment(deployment_arg):
     match deployment_arg:
         case "app1":
             print("Building " + deployment_arg + ".....")
-            os.system('cd ProjectsApp && cd financial-service-web && docker build -t financial-service-web .')
-            os.system('docker stack deploy node --compose-file docker-compose-microservices.yml --with-registry-auth')
+            os.system('cd microservices && cd financial-service-web && docker build -t financial-service-web .')
+            os.system('docker stack deploy node --compose-file docker-compose.yml --with-registry-auth')
         case "app2":
             print("Building " + deployment_arg + ".....")
-            os.system('cd ProjectsApp && cd pet-shop-web && docker build -t pet-shop-web .')
-            os.system('docker stack deploy node --compose-file docker-compose-microservices.yml --with-registry-auth')
+            os.system('cd microservices && cd pet-shop-web && docker build -t pet-shop-web .')
+            os.system('docker stack deploy node --compose-file docker-compose.yml --with-registry-auth')
 
         case "app3":
             print("Building " + deployment_arg + ".....")
-            os.system('cd ProjectsApp && cd selo-web && docker build -t selo-web .')
-            os.system('docker stack deploy node --compose-file docker-compose-microservices.yml --with-registry-auth')
+            os.system('cd microservices && cd selo-web && docker build -t selo-web .')
+            os.system('docker stack deploy node --compose-file docker-compose.yml --with-registry-auth')
         case "app4":
             print("Building " + deployment_arg + ".....")
-            os.system('cd ProjectsApp && cd php-sample-demo && docker build -t php-sample-demo .')
-            os.system('docker stack deploy node --compose-file docker-compose-microservices.yml --with-registry-auth')
+            os.system('cd microservices && cd php-sample-demo && docker build -t php-sample-demo .')
+            os.system('docker stack deploy node --compose-file docker-compose.yml --with-registry-auth')
 
         case "all":
             print("Building " + deployment_arg + ".....")
-            os.system('cd ProjectsApp && cd financial-service-web && docker build -t financial-service-web .')
-            os.system('cd ProjectsApp && cd pet-shop-web && docker build -t pet-shop-web .')
-            os.system('cd ProjectsApp && cd php-sample-demo && docker build -t php-sample-demo .')
-            os.system('cd ProjectsApp && cd selo-web && docker build -t selo-web .')
-            os.system('docker stack deploy node --compose-file docker-compose-microservices.yml --with-registry-auth')
+            os.system('cd microservices && cd financial-service-web && docker build -t financial-service-web .')
+            os.system('cd microservices && cd pet-shop-web && docker build -t pet-shop-web .')
+            os.system('cd microservices && cd php-sample-demo && docker build -t php-sample-demo .')
+            os.system('cd microservices && cd selo-web && docker build -t selo-web .')
+            os.system('docker stack deploy node --compose-file docker-compose.yml --with-registry-auth')
 
 
 def deploy():
