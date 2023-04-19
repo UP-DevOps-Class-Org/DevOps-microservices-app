24a35f02a2c53d0c1ca63be97be8a8a7976f7e08
Sithvothy KIV <34917417+sithvothykiv@users.noreply.github.com>
2023-04-19 09:26:46 +0700

    update app

--- microservices/AuthenticationApp/src/main/java/com/example/AuthenticationApp/model/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.example.AuthenticationApp.model;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
--- microservices/AuthenticationApp/src/main/java/com/example/AuthenticationApp/security/WebSecurityConfig.java
+++
@@ -1,5 +0,0 @@
-package com.example.AuthenticationApp.security;
-
-public class WebSecurityConfig {
-    
-}
