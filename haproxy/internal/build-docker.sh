e9431dfc1bb1e68912729e2dd2b55196a4beb54a
Sithvothy KIV <34917417+sithvothykiv@users.noreply.github.com>
2023-04-28 08:58:05 +0700

    modified:   haproxy/internal/Dockerfile 	modified:   haproxy/internal/build-docker.sh

--- haproxy/internal/build-docker.sh
+++ haproxy/internal/build-docker.sh
@@ -2,6 +2,6 @@
 
 docker build -t sithvothykiv/haproxy:1.7 .
 
-# if [ $? = 0 ]; then
-#     docker push sithvothykiv/haproxy:1.7
-# fi
+if [ $? = 0 ]; then
+    docker push sithvothykiv/haproxy:1.7
+fi
